8a72d5a9ebd1a3a91dec68683f5380a45e2d8c00
Leonardo Diaz <ldm@tt.ldiaz.lan>
2020-08-05 12:22:40 -0400

    Initial commit

---
+++ src/influxdb_data.py
@@ -0,0 +1,83 @@
+#!/bin/env python3
+import sys
+import argparse
+import json
+import os
+
+COLS = 4
+width = 72 // COLS
+
+
+def create_table(table, caption, style="border: 1px"):
+    if len(table) > 0:
+        result = [f'<table style="{style}"><caption>{caption}</caption>']
+        rows = max(1, len(table) // COLS)
+        counter = 0
+        for r in range(rows):
+            result.append("<tr>")
+            for c in range(COLS):
+                try:
+                    value = table[counter]
+                except IndexError:
+                    break
+                counter += 1
+                result.append(f"<td>{value}</td>")
+            result.append("</tr>")
+        result.append("</table>")
+    else:
+        result = [f"<p>No {caption} registered</p>"]
+    return '\n'.join(result)
+
+
+def main(infile, outfile):
+    # open data file
+    with open(infile) as fp:
+        foo = json.load(fp)
+
+    with open(outfile, "w") as fp:
+        fp.write("""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8"/>
+
+        <style>
+        body {margin-left: 2em;}
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        table {background-color: #F0F8EB}
+        th, td {padding: 5px;}
+        tr:nth-child(even) {background-color: #E5F7D2;}
+        </style>
+        </head>
+        <body>
+        """)
+
+        # pprint.pprint(foo)
+        dbases = foo.keys()
+
+        for dbase in dbases:
+            fp.write(f"\n\n<h1>Database: {dbase}</h1>")
+            tables = foo[dbase].keys()
+            for table in tables:
+                fp.write(f"\n<h2>Table (measurement): {table}</h2>")
+                cur_table = foo[dbase][table][0]
+                fp.write(create_table(cur_table['tags'], "Tags"))
+                fp.write(create_table(cur_table["fields"], "Field names"))
+
+        fp.write("</body></html>")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="JSON formatted input file")
+    parser.add_argument("--outfile", "-o", help="name for output file")
+
+    args = parser.parse_args()
+    print(args)
+    if args.outfile is not None:
+        args.outfile = os.path.splitext(args.infile)[0] + ".html"
+    print(args)
+    main(args.infile, args.outfile)
