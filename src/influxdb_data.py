df579bf2f837becc0a2c0cd93fdc9067c0c117ed
Leonardo Diaz <ldm@tt.ldiaz.lan>
2020-08-05 14:32:52 -0400

    Initial commit

--- src/influxdb_data.py
+++ src/influxdb_data.py
@@ -2,7 +2,11 @@
 import sys
 import argparse
 import json
+import logging
 import os
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+logger = logging.getLogger(__name__)
 
 COLS = 4
 width = 72 // COLS
@@ -34,50 +38,41 @@ def main(infile, outfile):
     with open(infile) as fp:
         foo = json.load(fp)
 
-    with open(outfile, "w") as fp:
-        fp.write("""
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="utf-8"/>
+    jenv = Environment(loader=FileSystemLoader(os.path.dirname(os.path.abspath(__file__))))
 
-        <style>
-        body {margin-left: 2em;}
-        table, th, td {
-          border: 1px solid black;
-          border-collapse: collapse;
-        }
-        table {background-color: #F0F8EB}
-        th, td {padding: 5px;}
-        tr:nth-child(even) {background-color: #E5F7D2;}
-        </style>
-        </head>
-        <body>
-        """)
+    dbases = foo.keys()
+    template = jenv.get_template('data.html')
+    logger.debug(template.render(dbases=foo, a_variable="AAA"))
+    # logger.debug(dbases)
 
-        # pprint.pprint(foo)
-        dbases = foo.keys()
+    #rendered = template.render(navigation=dbases, a_variable="AAA")
 
-        for dbase in dbases:
-            fp.write(f"\n\n<h1>Database: {dbase}</h1>")
-            tables = foo[dbase].keys()
-            for table in tables:
-                fp.write(f"\n<h2>Table (measurement): {table}</h2>")
-                cur_table = foo[dbase][table][0]
-                fp.write(create_table(cur_table['tags'], "Tags"))
-                fp.write(create_table(cur_table["fields"], "Field names"))
-
-        fp.write("</body></html>")
+    #logger.debug(rendered)
+    # for dbase in dbases:
+    #     fp.write(f"\n\n<h1>Database: {dbase}</h1>")
+    #     tables = foo[dbase].keys()
+    #     for table in tables:
+    #         fp.write(f"\n<h2>Table (measurement): {table}</h2>")
+    #         cur_table = foo[dbase][table][0]
+    #         fp.write(create_table(cur_table['tags'], "Tags"))
+    #         fp.write(create_table(cur_table["fields"], "Field names"))
+    #
+    # fp.write("</body></html>")
+    #
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)-4.4s %(module)-9.9s %(lineno)4d - %(message)s',
+                        level=logging.DEBUG)
+    logging.info("Starting %s", os.path.realpath(__file__))
+
     parser = argparse.ArgumentParser()
     parser.add_argument("infile", help="JSON formatted input file")
     parser.add_argument("--outfile", "-o", help="name for output file")
 
     args = parser.parse_args()
-    print(args)
+    logger.debug(args)
     if args.outfile is not None:
         args.outfile = os.path.splitext(args.infile)[0] + ".html"
-    print(args)
+    logger.debug(args)
     main(args.infile, args.outfile)
